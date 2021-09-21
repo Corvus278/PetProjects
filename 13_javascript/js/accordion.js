cde6150519e277ca88b1c47a16ad1f13f7059e37
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2021-09-21 17:10:06 +0300

    Сделал аккордион

---
+++ 13_javascript/js/accordion.js
@@ -0,0 +1,12 @@
+$(function () {
+  // Стандартная настройка
+  $(".questions__list").accordion({
+    active: true,
+    collapsible: true,
+    animate: 700,
+    heightStyle: "content"
+  });
+
+  // Персональная настройка
+
+});
